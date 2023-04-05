c7cd4fd67aa76ccb7c48526c097c41e9c7044ea0
Jace <jakridge2@gmail.com>
2023-04-05 16:34:55 +0000

    Finished Iteration 2!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_03_191047) do
+ActiveRecord::Schema.define(version: 2023_04_03_214514) do
 
   create_table "projects", force: :cascade do |t|
     t.text "restaurant"
