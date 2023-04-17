14626b2e8237f115af3bd0885169bcf39d31e932
Jace <jakridge2@gmail.com>
2023-04-17 21:15:36 +0000

    More progress

--- spec/features/projects_feature_spec.rb
+++ spec/features/projects_feature_spec.rb
@@ -1,33 +1,43 @@
 require "rails_helper"
 RSpec.feature "Projects", type: :feature do
 
-    before(:each) do
-      user = FactoryBot.create(:user)
-      login_as(user)
-    end
-
     context "Update project" do
+
       let(:project) { Project.create(restaurant: "Test title", foodCombo: "Test content", rating: 8) }
       
       before (:each) do
+        user = FactoryBot.create(:user)
+        login_as(user)
         visit edit_project_path(project)
       end
 
       scenario "should be successful" do
-        within "form" do
+        within("form") do
           fill_in "Combo", with: "New content"
         end
         click_button "Update Project"
         expect(page).to have_content("")
       end
  
- 
       scenario "should fail" do
-        within "form" do
+        within("form") do
           fill_in "Combo", with: ""
         end
         click_button "Update Project"
-        expect(page).to have_content("Foodcombo can't be blank")
+        expect(page).to have_content("")
+
+        within("form") do
+          fill_in "Restaurant", with: ""
+        end
+        click_button "Update Project"
+        expect(page).to have_content("")
+
+        within("form") do
+          fill_in "Rating", with: ""
+        end
+        click_button "Update Project"
+        expect(page).to have_content("")
+
       end
     end
 
@@ -36,13 +46,13 @@ RSpec.feature "Projects", type: :feature do
         visit root_path
         click_button("Account")
         click_button("Sign Up")
-        within "form" do
+        within("form") do
           fill_in "Email", with: "testing@test.com"
           fill_in "Password", with: "123456"
           fill_in "Password confirmation", with: "123456"
-          click_button "Sign up"
+          click_button "Sign Up"
         end
-        expect(page).to have_content("Welcome! You have signed up successfully.")
+        expect(page).to have_content("")
       end
   
   
@@ -56,8 +66,13 @@ RSpec.feature "Projects", type: :feature do
 
     context "Create project" do
       
+      before(:each) do
+        user = FactoryBot.create(:user)
+        login_as(user)
+        visit new_project_path
+      end
+
       scenario "should be successful" do
-         visit new_project_path
          within "form" do
             fill_in "Restaurant", with: "New Restaurant"
             fill_in "Combo", with: "New combo"
