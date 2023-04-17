2472de97f7a1c0bffc2a606e14b6f326f4f9bf7f
Jace <jakridge2@gmail.com>
2023-04-17 20:17:20 +0000

    More implementation of GE07

--- spec/features/projects_feature_spec.rb
+++ spec/features/projects_feature_spec.rb
@@ -1,15 +1,20 @@
 require "rails_helper"
 RSpec.feature "Projects", type: :feature do
+
+    before(:each) do
+      user = FactoryBot.create(:user)
+      login_as(user)
+    end
+
     context "Update project" do
       let(:project) { Project.create(restaurant: "Test title", foodCombo: "Test content", rating: 8) }
       
-      before(:each) do
+      before (:each) do
         visit edit_project_path(project)
       end
- 
- 
+
       scenario "should be successful" do
-        within("form") do
+        within "form" do
           fill_in "Combo", with: "New content"
         end
         click_button "Update Project"
@@ -18,11 +23,49 @@ RSpec.feature "Projects", type: :feature do
  
  
       scenario "should fail" do
-        within("form") do
+        within "form" do
           fill_in "Combo", with: ""
         end
         click_button "Update Project"
         expect(page).to have_content("Foodcombo can't be blank")
       end
     end
+
+    context "Login" do
+      scenario "should sign up" do
+        visit root_path
+        click_button("Account")
+        click_button("Sign Up")
+        within "form" do
+          fill_in "Email", with: "testing@test.com"
+          fill_in "Password", with: "123456"
+          fill_in "Password confirmation", with: "123456"
+          click_button "Sign up"
+        end
+        expect(page).to have_content("Welcome! You have signed up successfully.")
+      end
+  
+  
+      scenario "should log in" do
+        user = FactoryBot.create(:user)
+        login_as(user)
+        visit root_path
+        expect(page).to have_content("Logged in")
+      end
+    end
+
+    context "Create project" do
+      
+      scenario "should be successful" do
+         visit new_project_path
+         within "form" do
+            fill_in "Restaurant", with: "New Restaurant"
+            fill_in "Combo", with: "New combo"
+            fill_in "Rating", with: 8
+         end
+
+         click_button "Create Project"
+         expect(page).to have_content("")
+      end
+   end
 end
